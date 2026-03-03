3b885141367b93cead37f4f95530e0e2821f8aff
Eduardo Hirsh (Macbook pro) <ehirsh@outlook.com>
2026-03-03 17:57:34 -0300

    Ajustes de codigo y tabulacion

--- Sucesiones_Representacion_1D.py
+++ Sucesiones_Representacion_1D.py
@@ -6,74 +6,69 @@ tam_fuentes=12
 
 @np.vectorize
 def func_f(x: int):
-  return ((-1)**x)*(1/x)
+    return ((-1)**x)*(1/x)
 
 def Draw_Sucesion_1D(n , intervalo_x = [-0.05,1.05], intervalo_y = [-0.125,0.125],solo_ultimo = False, ocultar_etiquetas = False):
-  indices_suc= np.arange(1,n+1)
-
-  #! iniciar figura
-  fig , ax = plt.subplots(figsize=(12,1))
-  aux1=min(0,intervalo_x[0])
-  aux2=max(1,intervalo_x[1])
-  dif = aux2-aux1
-  ax.set_xlim(aux1-0.05*dif,aux2+0.05*dif)
-  ax.set_ylim(*intervalo_y)
-
-  #* dibujar ejes coordenados
-  ax.spines[["bottom"]].set_position(("data", 0))
-  ax.spines[["left", "top", "right"]].set_visible(False)
-  ax.plot(1, 0, ">", transform=ax.get_yaxis_transform(), clip_on=False, color = 'black')
-
-  # Graficar la función
-  if solo_ultimo:
-    sucesion = func_f(n)
-  else:
-    sucesion = func_f(indices_suc)
-
-  ax.scatter(sucesion,np.zeros_like(sucesion) , color='blue', s=30)
-
-  # etiquetas de los puntos
-  if not ocultar_etiquetas:
+    indices_suc= np.arange(1,n+1)
     if solo_ultimo:
-      ax.text(sucesion, 0.025 , f'$a_{{{n}}}$', fontsize=tam_fuentes, ha='center', va='bottom')
+        sucesion = func_f(n)
     else:
-      for i in range(n):
-        ax.text(sucesion[i], 0.025 , f'$a_{{{i+1}}}$', fontsize=tam_fuentes, ha='center', va='bottom')
+        sucesion = func_f(indices_suc)
 
-  # etiquetas de los valores en los ejes
-  etiquetas_x = np.arange(aux1, aux2+0.1*(aux2-aux1), 0.1*(aux2-aux1))
-  ax.set_xticks(etiquetas_x)
-  ax.set_yticks([])
+    #* iniciar figura
+    fig , ax = plt.subplots(figsize=(10,1))
 
-  # tamaño de fuentes en los ejes
-  ax.tick_params(axis='both', which='major', labelsize=tam_fuentes)
+    aux1=min(intervalo_x[0], np.min(sucesion))
+    aux2=max(intervalo_x[1], np.max(sucesion))
+    dif = aux2-aux1
+    ax.set_xlim(aux1-0.05*dif,aux2+0.05*dif)
+    ax.set_ylim(*intervalo_y)
 
-  return fig
+    #* Graficar la sucesión
+    ax.scatter(sucesion,np.zeros_like(sucesion) , color='blue', s=30)
+
+    #* etiquetas de los puntos
+    if not ocultar_etiquetas:
+        if solo_ultimo:
+            ax.text(sucesion, 0.025 , f'$a_{{{n}}}$', fontsize=tam_fuentes, ha='center', va='bottom')
+        else:
+            for i in range(n):
+                ax.text(sucesion[i], 0.025 , f'$a_{{{i+1}}}$', fontsize=tam_fuentes, ha='center', va='bottom')
+
+    #* etiquetas de los valores en los ejes
+    etiquetas_x = np.arange(aux1, aux2+0.1*(aux2-aux1), 0.1*(aux2-aux1))
+    ax.set_xticks(etiquetas_x)
+    ax.set_yticks([])
+
+    #* tamaño de fuentes en los ejes
+    ax.tick_params(axis='both', which='major', labelsize=tam_fuentes)
+
+    return fig
 
 
 def main():
-  # intervalo x
-  intervalo_x = [-1,1]
+    #* intervalo x
+    intervalo_x = [-1,1]
 
-  # cantidad numero de elementos de la sucesion
-  n=6
+    #* cantidad numero de elementos de la sucesion
+    n=6
 
-  #! Configuración de la página de Streamlit
-  st.set_page_config(page_title="Visualización 1D de una sucesión", layout="wide", initial_sidebar_state='expanded', page_icon=':material/line_axis:')
+    #! Configuración de la página de Streamlit
+    st.set_page_config(page_title="Visualización 1D de una sucesión", layout="wide", initial_sidebar_state='expanded', page_icon=':material/line_axis:')
 
-  #! Titulo
-  st.title('Visualización 1D de una sucesión')
+    #! Titulo
+    st.title('Visualización 1D de una sucesión')
 
-  #! Checkboxes para opciones de visualización
-  n = st.sidebar.number_input('indique el valor de $n$', min_value=1, value=n, step=1)
-  ocultar_etiquetas = st.sidebar.toggle('Ocultar etiquetas sucesión', value=False)
-  solo_ultimo = st.sidebar.toggle('Mostrar solo el término actual', value=False)
+    #! Checkboxes para opciones de visualización
+    n = st.sidebar.number_input('indique el valor de $n$', min_value=1, value=n, step=1)
+    ocultar_etiquetas = st.sidebar.toggle('Ocultar etiquetas sucesión', value=False)
+    solo_ultimo = st.sidebar.toggle('Mostrar solo el término actual', value=False)
 
-  #! Generar gráfico con spinner
-  with st.spinner('Generando gráfico...'):
-    fig = Draw_Sucesion_1D(n , intervalo_x, solo_ultimo=solo_ultimo, ocultar_etiquetas=ocultar_etiquetas)
-  st.pyplot(fig)
-  st.markdown(f'Grafico sucesión $a_n = \\dfrac{{(-1)^{{n}}}}{{n}}$')
+    #! Generar gráfico con spinner
+    with st.spinner('Generando gráfico...'):
+        fig = Draw_Sucesion_1D(n , intervalo_x, solo_ultimo=solo_ultimo, ocultar_etiquetas=ocultar_etiquetas)
+    st.pyplot(fig)
+    st.markdown(f'Grafico sucesión $a_n = \\dfrac{{(-1)^{{n}}}}{{n}}$')
 
 if __name__ == "__main__":
-  main()
+    main()
--- Sucesiones_Representacion_2D.py
+++ Sucesiones_Representacion_2D.py
@@ -6,82 +6,82 @@ tam_fuentes=12
 
 
 def func_f(x: float):
-  # return (-1)**x*(0.1+1/x)
-  return ((np.cos(x))**2)/x
+    # return (-1)**x*(0.1+1/x)
+    return ((np.cos(x))**2)/x
 
 def Draw_Sucesion_2D(n , intervalo_x = [-0.5,6.5], intervalo_y = [-0.125,1.125], ocultar_numeros = False, ocultar_etiquetas = False):
-  indices_suc= np.arange(1,n+1)
-  sucesion = func_f(indices_suc)
-  min_suc = np.min(sucesion)
-  max_suc = np.max(sucesion)
+    indices_suc= np.arange(1,n+1)
+    sucesion = func_f(indices_suc)
+    min_suc = np.min(sucesion)
+    max_suc = np.max(sucesion)
 
-  #! iniciar figura
-  fig , ax = plt.subplots(figsize=(10,5))
+    #! iniciar figura
+    fig , ax = plt.subplots(figsize=(10,5))
 
-  aux1_x=min(0,intervalo_x[0])
-  aux2_x=max(5,max(intervalo_x[1],n))
-  dif_x = aux2_x-aux1_x
-  rango_x = (aux1_x-0.05*dif_x,aux2_x+0.05*dif_x)
-  ax.set_xlim(rango_x)
+    aux1_x=min(0,intervalo_x[0])
+    aux2_x=max(5,max(intervalo_x[1],n))
+    dif_x = aux2_x-aux1_x
+    rango_x = (aux1_x-0.05*dif_x,aux2_x+0.05*dif_x)
+    ax.set_xlim(rango_x)
 
-  aux1_y=min(min_suc,intervalo_y[0])
-  aux2_y=max(max_suc,intervalo_y[1])
-  dif_y = aux2_y-aux1_y
-  rango_y = (aux1_y-0.05*dif_y,aux2_y+0.05*dif_y)
-  ax.set_ylim(rango_y)
+    aux1_y=min(min_suc,intervalo_y[0])
+    aux2_y=max(max_suc,intervalo_y[1])
+    dif_y = aux2_y-aux1_y
+    rango_y = (aux1_y-0.05*dif_y,aux2_y+0.05*dif_y)
+    ax.set_ylim(rango_y)
 
-  #* dibujar ejes coordenados
-  ax.spines[["left", "bottom"]].set_position(("data", 0))
-  ax.spines[["top", "right"]].set_visible(False)
-  ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), clip_on=False)
-  ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False)
+    #* dibujar ejes coordenados
+    ax.spines[["left", "bottom"]].set_position(("data", 0))
+    ax.spines[["top", "right"]].set_visible(False)
+    ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), clip_on=False)
+    ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False)
 
-  # Graficar la función
-  ax.scatter(indices_suc, sucesion, color='blue', s=10)
+    # Graficar la función
+    ax.scatter(indices_suc, sucesion, color='blue', s=10)
 
-  # etiquetas de los puntos
-  if not ocultar_etiquetas:
-    desp = 0.04*(rango_y[1]-rango_y[0])
-    for i in range(n):
-      ax.text(indices_suc[i]+0.05, sucesion[i]+0.02 if sucesion[i] >= 0 else sucesion[i]-desp, f'$a_{{{i+1}}}$', fontsize=tam_fuentes, ha='center', va='bottom')
+    # etiquetas de los puntos
+    if not ocultar_etiquetas:
+        desp = 0.04*(rango_y[1]-rango_y[0])
+        for i in range(n):
+            ax.text(indices_suc[i]+0.05, sucesion[i]+0.02 if sucesion[i] >= 0 else sucesion[i]-desp, f'$a_{{{i+1}}}$', fontsize=tam_fuentes, ha='center', va='bottom')
 
-  # etiquetas de los valores en los ejes
-  if ocultar_numeros:
-    ax.set_xticks([])
-  else:
-    ax.set_xticks(indices_suc)
-  ax.set_yticks(np.arange(aux1_y, aux2_y+0.1*(aux2_y-aux1_y), 0.1*(aux2_y-aux1_y)))
+    # etiquetas de los valores en los ejes
+    if ocultar_numeros:
+        ax.set_xticks([])
+    else:
+        ax.set_xticks(indices_suc)
+    ax.set_yticks(np.arange(aux1_y, aux2_y+0.1*(aux2_y-aux1_y), 0.1*(aux2_y-aux1_y)))
 
-  # tamaño de fuentes en los ejes
-  ax.tick_params(axis='both', which='major', labelsize=tam_fuentes)
+    # tamaño de fuentes en los ejes
+    ax.tick_params(axis='both', which='major', labelsize=tam_fuentes)
 
-  return fig
+    return fig
 
 
 def main():
-  # intervalos x e y
-  intervalo_x = [0,6]
-  intervalo_y = [0,0.5]
+    # intervalos x e y
+    intervalo_x = [0,6]
+    intervalo_y = [0,0.5]
 
-  # cantidad numero de elementos de la sucesion
-  n=6
+    # cantidad numero de elementos de la sucesion
+    n=6
 
-  #! Configuración de la página de Streamlit
-  st.set_page_config(page_title="Visualización 2D de una sucesión", layout="wide", initial_sidebar_state='expanded', page_icon=':material/line_axis:')
+    #! Configuración de la página de Streamlit
+    st.set_page_config(page_title="Visualización 2D de una sucesión", layout="wide", initial_sidebar_state='expanded', page_icon=':material/line_axis:')
 
-  #! Titulo
-  st.title('Visualización 2D de una sucesión')
+    #! Titulo
+    st.title('Visualización 2D de una sucesión')
 
-  #! Checkboxes para opciones de visualización
-  n = st.sidebar.number_input('indique el valor de $n$', min_value=1, value=n, step=1)
-  ocultar_etiquetas = st.sidebar.toggle('Ocultar etiquetas sucesión', value=False)
-  ocultar_numeros = st.sidebar.toggle('Ocultar etiquetas eje $x$', value=False)
+    #! Checkboxes para opciones de visualización
+    n = st.sidebar.number_input('indique el valor de $n$', min_value=1, value=n, step=1)
+    ocultar_etiquetas = st.sidebar.toggle('Ocultar etiquetas sucesión', value=False)
+    ocultar_numeros = st.sidebar.toggle('Ocultar etiquetas eje $x$', value=False)
 
-  #! Generar gráfico con spinner
-  with st.spinner('Generando gráfico...'):
-    fig = Draw_Sucesion_2D(n , intervalo_x, intervalo_y, ocultar_numeros=ocultar_numeros, ocultar_etiquetas=ocultar_etiquetas)
-  st.pyplot(fig)
-  st.markdown(r'Grafico sucesión $a_n = \dfrac{\cos^2(n)}{n}$')
+    #! Generar gráfico con spinner
+    with st.spinner('Generando gráfico...'):
+        fig = Draw_Sucesion_2D(n , intervalo_x, intervalo_y, ocultar_numeros=ocultar_numeros, ocultar_etiquetas=ocultar_etiquetas)
+    st.pyplot(fig)
+    st.markdown(r'Grafico sucesión $a_n = \dfrac{\cos^2(n)}{n}$')
 
 if __name__ == "__main__":
-  main()
+    main()
